ff29996b586109422874cdfd9d42855fee3b5aa8
ianhernandez <ianhernandez23@gmail.com>
2015-12-23 12:02:28 -0800

    major updates sorry

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,19 +1,35 @@
 module.exports = function(grunt) {
-  grunt.loadNpmTasks('grunt-shopify-sass');
+    grunt.loadNpmTasks('grunt-shopify');
+    grunt.loadNpmTasks('grunt-shopify-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
 
-  grunt.initConfig({
-    shopify_sass: {
-          theme: {
-              src: 'styles/theme.scss',
-              dest: 'assets/theme.scss.liquid'
-          }
+    grunt.initConfig({
+      shopify_sass: {
+            theme: {
+                src: 'styles/theme.scss',
+                dest: 'assets/theme.scss.liquid'
+            }
       },
-  });
+      shopify: {
+          options: {
+            api_key: "694800fb550bc85c81a38e35ff166cb5",
+            password: "8b9961913ed70ff36af39ced770dbb4e",
+            url: "v-for-vape.myshopify.com",
+          }
+        },
+        watch: {
+            shopify_sass: {
+                files: ['styles/*'],
+                tasks: ['shopify_sass']
+            },
+            shopify: {
+                files: ['assets/*.*', 'themes/*.*', 'snippets/*.*', 'config/*.*', 'layout/*.*', 'locale/*.*', 'spec/*.*', 'templates/*.*', 'templates/customers/*.*'],
+                tasks: ['shopify'],
+                events: true,
+                spawn: true
+            },
+        }
+    });
 
-
-
-
-  // Default task(s).
-  grunt.registerTask('default', ['shopify_sass']);
-
+    grunt.registerTask('default', ['watch']);
 };
